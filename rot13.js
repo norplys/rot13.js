f593963271308ebac4e1f7beede91413e8952c11
norplys <joywinata88@gmail.com>
2023-04-20 20:14:58 +0700

    first commit

---
+++ rot13.js
@@ -0,0 +1,16 @@
+function rot13(str) {
+    let words = str.split("");
+    let translate = words.map(a => {
+      const myRegex = /[A-Za-z]/;
+      if(myRegex.test(a) == false){
+        return a.charCodeAt(0);
+      } return (a.charCodeAt(0) + 13 - 65) % 26 + 65
+    });
+    let result = translate.map(a => String.fromCharCode(a)).join("")
+    console.log(result)
+    return result;
+  
+  }
+  
+  rot13("SERR CVMMN!");
+  
